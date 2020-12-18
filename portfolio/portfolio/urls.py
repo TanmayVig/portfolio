711a08e70c0aa589f827b14535a90f7ecfeada07
Tanmay Vig <tanmayvij11@gmail.com>
2020-12-19 01:00:07 +0530

    made landing and about page, changed theme and shifted everything to components.

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('Tanmay_Creates.urls'))
+    path('',include('Tanmay_Creates.urls')),
+    path('favicon.ico',RedirectView.as_view(url=staticfiles_storage.url('media/logo_final_1.png')))
 ]
